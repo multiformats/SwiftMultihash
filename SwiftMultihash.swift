bee847d7bc1ba9945c8fbb31935d51cd8b93d1ca
Teo <m@sartori.dk>
2015-05-19 23:10:14 +0200

    Add all but the Blake functions.

--- SwiftMultihash.swift
+++ SwiftMultihash.swift
@@ -8,22 +8,18 @@
 
 import Foundation
 
+
 /// Errors
 let ErrDomain = "MultiHashDomain"
-let
-    ErrUnknownCode      = NSError(domain: ErrDomain, code: -1, userInfo: [NSLocalizedDescriptionKey : "Unknown multihash code"]),
-    ErrTooShort         = NSError(domain: ErrDomain, code: -2, userInfo: [NSLocalizedDescriptionKey : "Multihash too short. Must be > 3 bytes"]),
-    ErrTooLong          = NSError(domain: ErrDomain, code: -3, userInfo: [NSLocalizedDescriptionKey : "Multihash too long. Must be < 129 bytes"]),
-ErrLenNotSupported  = NSError(domain: ErrDomain, code: -4, userInfo: [NSLocalizedDescriptionKey : "Multihash does not yet support digests longer than 127 bytes"])
+public let
+ErrUnknownCode      = NSError(domain: ErrDomain, code: -1, userInfo: [NSLocalizedDescriptionKey : "Unknown multihash code"]),
+ErrTooShort         = NSError(domain: ErrDomain, code: -2, userInfo: [NSLocalizedDescriptionKey : "Multihash too short. Must be > 3 bytes"]),
+ErrTooLong          = NSError(domain: ErrDomain, code: -3, userInfo: [NSLocalizedDescriptionKey : "Multihash too long. Must be < 129 bytes"]),
+ErrLenNotSupported  = NSError(domain: ErrDomain, code: -4, userInfo: [NSLocalizedDescriptionKey : "Multihash does not yet support digests longer than 127 bytes"]),
+ErrHexFail  = NSError(domain: ErrDomain, code: -5, userInfo: [NSLocalizedDescriptionKey : "Error occurred in hex conversion."])
 
-struct ErrInconsistentLen {
-    let dm: DecodedMultihash
-}
-
-extension ErrInconsistentLen {
-    func error() -> String {
-        return "Multihash length inconsistent: \(dm)"
-    }
+func ErrInconsistentLen(dm: DecodedMultihash) -> NSError {
+        return NSError(domain: ErrDomain, code: -6, userInfo: [NSLocalizedDescriptionKey : "Multihash length inconsistent: \(dm)"])
 }
 
 let
@@ -61,25 +57,68 @@ let DefaultLengths: [Int : Int] = [
     BLAKE2S     : 32
 ]
 
-struct DecodedMultihash {
+public struct DecodedMultihash {
     let code    : Int
     let name    : String
     let length  : Int
     let digest  : [uint8]
 }
 
-typealias Multihash = [uint8]
+public struct Multihash {
+    let value: [uint8]
+}
 
-//extension Multihash {
-////    func hexString() -> String {
-////        return
-////    }
-//}
+extension Multihash {
+    public func hexString() -> String {
+        return Hex.encodeToString(value)
+    }
+    
+    public func string() -> String {
+        return self.hexString()
+    }
+}
 
+func fromHexString(theString: String) -> (Multihash?, NSError?) {
+    if let buf = Hex.decodeString(theString) {
+        return cast(buf)
+    }
+    return (nil, ErrHexFail)
+}
+
+public func cast(buf: [uint8]) -> (Multihash?, NSError?) {
+    let (dm, err) = decode(buf)
+    
+    if err != nil {
+        return (nil, err)
+    }
+    
+    if validCode(dm!.code) == false {
+        return (nil,ErrUnknownCode)
+    }
+    
+    return (Multihash(value: buf),nil)
+}
+
+public func decode(buf: [uint8]) -> (DecodedMultihash?, NSError?) {
+
+    if buf.count < 3 {
+        return (nil, ErrTooShort)
+    }
+    if buf.count > 129 {
+        return (nil, ErrTooLong)
+    }
+
+    let dm = DecodedMultihash(code: Int(buf[0]), name: Codes[Int(buf[0])]!, length: Int(buf[1]), digest: Array(buf[2...buf.count]))
+    
+    if dm.digest.count != dm.length {
+        return (nil, ErrInconsistentLen(dm))
+    }
+    return (dm, nil)
+}
 
 /// Encode a hash digest along with the specified function code
 /// Note: The length is derived from the length of the digest.
-func encode(buf: [uint8], code: Int?) -> ([uint8]?, NSError?) {
+public func encode(buf: [uint8], code: Int?) -> ([uint8]?, NSError?) {
 
     if validCode(code) == false {
         return (nil,ErrUnknownCode)
@@ -119,4 +158,49 @@ func validCode(code: Int?) -> Bool {
 /// AppCode checks whether a multihash code is part of the App range.
 func appCode(code: Int) -> Bool {
     return code >= 0 && code < 0x10
+}
+
+
+
+// Move this into a hex lib
+public struct Hex {
+    public static func encodeToString(hexBytes: [uint8]) -> String {
+        var outString = ""
+        for val in hexBytes {
+            outString += String(val, radix: 16)
+        }
+        return outString
+    }
+
+    private static func trimString(theString: String) -> String? {
+        let trimmedString = theString.stringByTrimmingCharactersInSet(NSCharacterSet(charactersInString: "<> ")).stringByReplacingOccurrencesOfString(" ", withString: "")
+        
+        // make sure the cleaned up string consists solely of hex digits, and that we have even number of them
+        
+        var error: NSError?
+        let regex = NSRegularExpression(pattern: "^[0-9a-f]*$", options: .CaseInsensitive, error: &error)
+        let found = regex?.firstMatchInString(trimmedString, options: nil, range: NSMakeRange(0, count(trimmedString)))
+        if found == nil || found?.range.location == NSNotFound || count(trimmedString) % 2 != 0 {
+            return nil
+        }
+        
+        return trimmedString
+    }
+
+    public static func decodeString(hexString: String) -> [uint8]? {
+        
+        if let data = NSMutableData(capacity: count(hexString) / 2) {
+            
+            for var index = hexString.startIndex; index < hexString.endIndex; index = index.successor().successor() {
+                let byteString = hexString.substringWithRange(Range<String.Index>(start: index, end: index.successor().successor()))
+                let num = UInt8(byteString.withCString { strtoul($0, nil, 16)})
+                data.appendBytes([num] as [UInt8], length: 1)
+            }
+            var outBuf = [uint8](count: data.length, repeatedValue: 0x0)
+            data.getBytes(&outBuf, length: data.length)
+            
+            return outBuf
+        }
+        return nil
+    }
 }
--- SwiftMultihashTests/SwiftMultihashTests.swift
+++ SwiftMultihashTests/SwiftMultihashTests.swift
@@ -8,8 +8,38 @@
 
 import Cocoa
 import XCTest
+import SwiftMultihash
+
+struct TestCase {
+    let hex     : String
+    let code    : Int
+    let name    : String
+}
+
+extension TestCase {
+    func MultiHash() -> (Multihash?, NSError?) {
+        if let ob = Hex.decodeString(hex) {
+            var b = [uint8](count: ob.count, repeatedValue: 0x0)
+            b[0] = uint8(code)
+            b[1] = uint8(ob.count)
+            b.extend(ob)
+            return cast(b)
+        } else {
+            return (nil, ErrHexFail)
+        }
+        
+    }
+}
 
 class SwiftMultihashTests: XCTestCase {
+    
+    
+    let testCases = [
+        TestCase(hex: "0beec7b5ea3f0fdbc95d0dd47f3c5bc275da8a33", code: 0x11, name: "sha1"),
+        TestCase(hex: "0beec7b5", code: 0x11, name: "sha1"),
+        TestCase(hex: "2c26b46b68ffc68ff99b453c1d30413413422d706483bfa0f98a5e886266e7ae", code: 0x12, name: "sha2-256"),
+        TestCase(hex: "2c26b46b", code: 0x12, name: "sha2-256")
+    ]
     
     override func setUp() {
         super.setUp()
@@ -25,6 +55,30 @@ class SwiftMultihashTests: XCTestCase {
         // This is an example of a functional test case.
         XCTAssert(true, "Pass")
     }
+
+    func testEncode() {
+        for tc in testCases {
+            let ob = Hex.decodeString(tc.hex)
+            if ob == nil {
+                XCTFail("Hex decodeString failed.")
+                continue
+            }
+            
+            var pre: [uint8] = [0,0]
+            pre[0] = uint8(tc.code)
+            pre[1] = uint8(ob!.count)
+            let nb = ob! + pre
+            encode()
+            
+            let (encC, err) = encode(pre, tc.code)
+//            if encC == nil {
+//                XCTFail("Hex decodeString failed.")
+//                continue
+//            }
+        }
+        XCTAssert(true, "Pass")
+    }
+    
     
     func testPerformanceExample() {
         // This is an example of a performance test case.
