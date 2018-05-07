7e1d6ef85354c0d947ec25ab9779a16d5f88839d
Teo Sartori <m@sartori.dk>
2018-05-07 14:43:15 +0200

    debug print buffer

--- SwiftMultihash.swift
+++ SwiftMultihash.swift
@@ -146,6 +146,7 @@ public func decodeBuf(_ buf: [UInt8]) throws -> DecodedMultihash {
     
     if dm.digest.count != dm.length {
         let b0 = Int(buf[0])
+        print("the buf is \(String(describing: String(bytes: buf, encoding: .utf8)))")
         print("SwiftMultihash decodeBuf error: buf[0] is \(b0), and code is \(String(describing: Codes[b0])) and buf[1] is \(buf[1])")
         throw MultihashError.inconsistentLength(dm.length)
     }
