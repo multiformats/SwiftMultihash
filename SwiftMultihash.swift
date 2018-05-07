066d3b38ec3526a50e9257e7a2040d3f07e290a2
Teo Sartori <m@sartori.dk>
2018-05-07 17:16:13 +0200

    Remove varint stuff

--- SwiftMultihash.swift
+++ SwiftMultihash.swift
@@ -146,13 +146,15 @@ public func decodeBuf(_ buf: [UInt8]) throws -> DecodedMultihash {
 
     let dm = DecodedMultihash(code: Int(buf[0]), name: Codes[Int(buf[0])], length: Int(buf[1]), digest: Array(buf[2..<buf.count]))
     
-    let b0 = varInt(buf) //Int(buf[0])
-    print("The var int read is \(b0.0) and was \(b0.1) bytes")
+    let strbuf = buf.map { String(format:"%02X ", $0) }.joined()
+    print("the buf is \(strbuf)")
+
+//    let b0 = varInt(buf) //Int(buf[0])
+//    print("The var int read is \(b0.0) and was \(b0.1) bytes")
 
     if dm.digest.count != dm.length {
-        let strbuf = buf.map { String(format:"%02X ", $0) }.joined()
-        print("the buf is \(strbuf)")
-//        print("SwiftMultihash decodeBuf error: buf[0] is \(b0), and code is \(String(describing: Codes[b0])) and buf[1] is \(buf[1])")
+        let b0 = Int(buf[0])
+        print("SwiftMultihash decodeBuf error: buf[0] is \(b0), and code is \(String(describing: Codes[b0])) and buf[1] is \(buf[1])")
         throw MultihashError.inconsistentLength(dm.length)
     }
 
