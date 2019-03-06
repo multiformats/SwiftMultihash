b8522493b95ec1a0e07c2da4bc88d47b141803ac
decanus <dean.eigenmann@icloud.com>
2019-03-06 23:56:20 +0100

    updated

--- Package.swift
+++ Package.swift
@@ -12,6 +12,7 @@ let package = Package(
             targets: ["SwiftMultihash"]),
     ],
     dependencies: [
+	.package(url: "https://github.com/NeoTeo/VarInt.git", from: "0.2.1")
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
     ],
